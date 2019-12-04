a1b9713fce58b835df47b0a4b8a4fd0efa4b20f1
JoeLiu1321 <angrybirdliu@gmail.com>
2019-12-04 15:36:54 +0800

    change css

--- src/app/patient-manager/patient-manager.component.ts
+++ src/app/patient-manager/patient-manager.component.ts
@@ -1,6 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import {FHIRProxyService} from '../fhir-proxy/fhirproxy.service';
 import { Router } from '@angular/router'
+import {MatPaginator} from '@angular/material/paginator';
+import {MatTableDataSource} from '@angular/material/table';
+
+export interface PeriodicElement {
+   diagnosis:string[],
+   organization:string,
+   time:{
+      start:string,
+      end:string
+   }
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {diagnosis: ['diagnosis1', 'diagnosis2'],  organization: 'organization', time:{start:'Test1', end:'Test2'}},
+  {diagnosis: ['diagnosis3', 'diagnosis2'],  organization: 'organization', time:{start:'Test1', end:'Test2'}},
+  {diagnosis: ['diagnosis5', 'diagnosis2'],  organization: 'organization', time:{start:'Test1', end:'Test2'}},
+  {diagnosis: ['diagnosis7', 'diagnosis2'],  organization: 'organization', time:{start:'Test1', end:'Test2'}},
+  {diagnosis: ['diagnosis9', 'diagnosis2'],  organization: 'organization', time:{start:'Test1', end:'Test2'}},
+  {diagnosis: ['diagnosis11', 'diagnosis2'],  organization: 'organization', time:{start:'Test1', end:'Test2'}},
+  {diagnosis: ['diagnosis13', 'diagnosis2'],  organization: 'organization', time:{start:'Test1', end:'Test2'}},
+  {diagnosis: ['diagnosis15', 'diagnosis2'],  organization: 'organization', time:{start:'Test1', end:'Test2'}},
+  {diagnosis: ['diagnosis17', 'diagnosis2'],  organization: 'organization', time:{start:'Test1', end:'Test2'}},
+  
+];
+
 @Component({
   selector: 'app-patient-manager',
   templateUrl: './patient-manager.component.html',
@@ -9,8 +34,12 @@ import { Router } from '@angular/router'
 export class PatientManagerComponent implements OnInit {
   constructor(private fhir:FHIRProxyService, private router:Router) { }
 
+  displayedColumns: string[] = ['diagnosis', 'organization', 'time.start', 'time.end'];
+  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
+  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
+  display = false;
   ngOnInit() {
-    
+    this.dataSource.paginator = this.paginator;
   }
 
 
